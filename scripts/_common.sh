430e6b3a226a99dfe66395457130babc81b4466f
Steve Marion <steve@smarion.me>
2025-08-28 22:46:31 +0200

    add start stop service

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,16 @@
 # PERSONAL HELPERS
 #=================================================
 
+start_mc_service() {
+    ynh_systemd_action --service_name=$app --action=start --log_path=systemd --line_match=".*INFO] Server started."
+}
+
+stop_mc_service() {
+    ynh_systemd_action --service_name=$app --action=stop --log_path=systemd --line_match=".*Stopped minecraft_bedrock.service.*"
+}
+
+
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
